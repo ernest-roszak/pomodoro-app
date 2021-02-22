205573d3e5c34251de7ce0bc5f932be5d45fd1d8
Ernest Roszak <ernest.roszak89@gmail.com>
2021-02-22 17:09:16 +0100

    added new component

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 1px solid #ec5822;
+  border-radius: 5px;
+  height: 25px;
+  margin-bottom: 20px;
+  padding: 5px;
+  filter: ${({ isBlur }) => (isBlur ? 'blur(2px) grayscale(1)' : 'none')};
+`;
+
+const Bar = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.orangered};
+`;
+
+export const ProgressBar = ({ process = 30 }) => {
+  return (
+    <Container>
+      <Bar style={{ width: `${process}%` }}></Bar>
+    </Container>
+  );
+};
